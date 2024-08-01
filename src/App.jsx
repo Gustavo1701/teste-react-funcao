6b7b5f7261c89b760ef4aaefa1fb2bac042077d3
Gustavo1701 <gustavojr.lg.10@gmail.com>
2024-07-31 21:46:36 -0300

    Atualizando codigo

--- src/App.jsx
+++ src/App.jsx
@@ -2,90 +2,129 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './input.css';
 
 function App() {
   const [resultado, setresultado] = useState('')
 
-
-
-  function somarDoisNumeros() {
+  function pedirNumero() {
     let num1 = prompt("Informe o valor 1");
     let num2 = prompt("Informe o valor 2");
 
     num1 = parseInt(num1);
     num2 = parseInt(num2);
+  }
 
+  function somarDoisNumeros() {
+
+    pedirNumero();
     const resultado = num1 + num2;
 
-    setresultado(resultado);
-    console.log(resultado);
+    setResultado(num1 + num2);
 
   }
 
   function diminuirDoisNumeros() {
-    let num1 = prompt("Informe o valor 1");
-    let num2 = prompt("Informe o valor 2");
-
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
+    pedirNumero();
 
     const resultado = num1 - num2;
 
-    setresultado(resultado);
-    console.log(resultado);
+    document.getElementById('spanResultado').innerHTML = resultado;
+
 
   }
 
   function multiplicarDoisNumeros() {
-    let num1 = prompt("Informe o valor 1");
-    let num2 = prompt("Informe o valor 2");
-
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
+    pedirNumero();
 
     const resultado = num1 * num2;
 
-    setresultado(resultado);
-    console.log(resultado);
+    document.getElementById('spanResultado').innerHTML = resultado;
+
 
   }
 
   function dividirDoisNumeros() {
-    let num1 = prompt("Informe o valor 1");
-    let num2 = prompt("Informe o valor 2");
-
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
+    pedirNumero();
 
     const resultado = num1 / num2;
 
-    setresultado(resultado);
-    console.log(resultado);
+    document.getElementById('spanResultado').innerHTML = resultado;
 
   }
 
-  const limparResultado = () => {
-    setresultado(null);
+  function descobrirIMC() {
+    let altura = prompt("Informe sua altura em metros");
+    let peso = prompt("Informe seu peso em kg");
+    altura = parseFloat(altura);
+    peso = parseFloat(peso);
+    let imc = peso / (altura * altura);
+    document.getElementById('resultadoIMC').innerHTML = imc;
 
+    if (imc <= 18.5) {
+      document.getElementById('resultadoIMC').innerHTML = "Abaixo do peso";
+    } else if (imc > 18.5 && imc <= 24.9) {
+      document.getElementById('resultadoIMC').innerHTML = "Peso normal";
+    } else if (imc > 25 && imc <= 29.9) {
+      document.getElementById('resultadoIMC').innerHTML = "Sobrepeso";
+    } else if (imc > 30 && imc <= 34.9) {
+      document.getElementById('resultadoIMC').innerHTML = "Obesidade grau I";
+    } else if (imc > 35 && imc <= 39.9) {
+      document.getElementById('resultadoIMC').innerHTML = "Obesidade grau II";
+    } else {
+      document.getElementById('resultadoIMC').innerHTML = "Obesidade grau III";
+    }
+
+  }
+
+
+  const limparResultado = () => {
+    document.getElementById('spanResultado').innerHTML = " ";
+    document.getElementById('resultadoIMC').innerHTML = " ";
   }
 
 
 
   return (
     <>
-      <div>
-        <button onClick={somarDoisNumeros}>+</button>
+      <div className="container">
+        <div className="row">
+          <form>
 
-        <button onClick={diminuirDoisNumeros}>-</button>
+            <div>
+              <button onClick={somarDoisNumeros}>+</button>
 
-        <button onClick={multiplicarDoisNumeros}>X</button>
+              <button onClick={diminuirDoisNumeros}>-</button>
 
-        <button onClick={dividirDoisNumeros}>/</button>
+              <button onClick={multiplicarDoisNumeros}>X</button>
+
+              <button onClick={dividirDoisNumeros}>/</button>
+            </div>
+
+            <div id='divResultado'>
+              <h1>O resultado é: <span id='spanResultado'></span> </h1>
+            </div>
+          </form>
+
+        </div>
+
+        <div className="row">
+          <form>
+            <div id='imc'>
+              <h1>Quer descobrir seu IMC?</h1>
+              <button onClick={descobrirIMC}>Clique aqui</button>
+              <h3>Seu IMC é: <span id='resultadoIMC'></span></h3>
+            </div>
+
+          </form>
+        </div>
+
+
       </div>
-      <div>
-        <h1>O resultado é: {resultado}</h1>
-      </div>
-      <button onClick={limparResultado}>Limpar</button>
+
+
+      <button onClick={limparResultado}>Limpar todos os resultados</button>
+
     </>
   )
 }
